4c2907ead5d24a6d75ad4ca275eb638ab43244ae
rens-kloosterman <rens.kloosterman@student.fontys.nl>
2022-10-03 13:01:13 +0200

    INIT project

---
+++ src/main/java/com/kwetter/tweetservice/TweetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kwetter.tweetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetserviceApplication.class, args);
+	}
+
+}
